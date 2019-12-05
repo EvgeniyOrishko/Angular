f66bff3b8769fc62008402df7c6dcd4e60d9fe9f
ev.orishko <orishko1990@gmail.com>
2019-12-05 13:43:53 +0200

    get all palanets and peoples

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -28,11 +28,11 @@ export class HomeComponent implements OnInit {
       .subscribe((data: PeopleRequest) => {
         this.dataSource.data = data.results;
         this.members = data.results;
-        this.getDataForFirstPage( data.results );
+        this.getDataForFirstPage( data.results, this.getAllPlanets );
       });
   }
 
-    getDataForFirstPage(list: Member[] ) {
+    getDataForFirstPage(list: Member[]) {
       Promise.all(
         list.map( item => {
             if( !this.planets[item.homeworld] ) {
@@ -50,9 +50,76 @@ export class HomeComponent implements OnInit {
           .then(data => {
             this.dataSource.data.forEach( item =>  item.homeWorldName = this.planets[item.homeworld].name )
             this.dataSource.sort = this.sort;
+            this.getAllPlanets();
           })
         .catch((err) =>  console.log(err.message));
   }
+
+
+  getAllPlanets() {
+    Promise.all(
+      [1,2,3,4,5,6,7].map( item => {
+
+        return this.apiService.fetchPr( 'https://swapi.co/api/planets/?page=' + item )
+          .then( data => {
+            return data.results;})
+          .catch( () => item );
+
+      }))
+      .then(data => {
+        console.error( 'PLANETS', data )
+
+        data.forEach( planets => planets.forEach( (planet : Planet) =>  this.planets[ planet.url ] = planet  ) );
+
+         this.getAllPeoples();
+
+      })
+      .catch(function(err) {
+        console.log(err.message); // some coding error in handling happened
+      });
+  }
+
+  getAllPeoples( ) {
+
+    Promise.all(
+      [ 2,3,4,5,6,7,8,9].map( item => {
+
+        return this.apiService.fetchPr( 'https://swapi.co/api/people/?page=' + item )
+          .then( data => {
+
+            return data.results})
+          .catch( () => item.homeworld );
+
+      }))
+      .then(data => {
+        // console.error( 'Peoples', data );
+
+        const restPeoples = data.reduce( (acc, item) => [...acc, ...item] );
+        restPeoples.forEach( (person: Member) => person.homeWorldName = this.planets[ person.homeworld || {} ].name )
+
+
+        console.warn( 'THIsPLANET', this.members);
+
+
+
+        this.dataSource.data = [ ...this.dataSource.data,  ...restPeoples] ;
+        this.dataSource.sort = this.sort;
+
+
+
+
+      })
+
+      .catch(function(err) {
+        console.log(err.message); // some coding error in handling happened
+      });
+  }
+
+
+
+
+
+
 
 
 
